3d394757b4b6660dac04ea27debe3193eaea14ba
Robert Mendoza <robmrivas@gmail.com>
2024-02-29 20:11:41 -0300

    render server

--- src/assets/utils.js
+++ src/assets/utils.js
@@ -1,5 +1,5 @@
 async function Counter (counterType) {
-    await fetch(`porfoliobackend-mb59kr72o-roberts-projects-2d1ea26b.vercel.app/counter/${counterType}`, {
+    await fetch(`https://porfoliobackend-ep8q.onrender.com${counterType}`, {
     method: 'GET', 
     headers: {
       },
